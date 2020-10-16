d0ec1088cc23fd5e11e6d0c51659259c5a9a9f19
Joshua Jainga <jjainga@gmail.com>
2020-10-15 18:26:12 -0700

    added router

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Createing data for the burgers tables --
+INSERT INTO burgers(burger_name, devoured) VALUES("Double Cheese Burger", true);
+INSERT INTO burgers(burger_name, devoured) VALUES("Whopper", true);
+INSERT INTO burgers(burger_name, devoured) VALUES("Big Mac", false);
+INSERT INTO burgers(burger_name, devoured) VALUES("Butttery Jack", false);
+
